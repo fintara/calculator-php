d955e4fb0719cd1858733c9827d1ce95f40abc8f
fintara <ccovedenski@gmail.com>
2014-02-18 23:55:10 +0200

    Additional assertions for getToken

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -17,8 +17,14 @@ class CalculatorTest extends \PHPUnit_Framework_TestCase {
     }
 
     public function testGetToken() {
-        $this->_calculator->setExpression("1+2-3");
+        $this->_calculator->setExpression('1+2-3');
         $this->assertEquals(['1', '+', '2', '-', '3'], $this->_calculator->getTokens());
+
+        $this->_calculator->setExpression('-5*(-5+1)');
+        $this->assertEquals(['-5', '*', '(', '-5', '+', '1', ')'], $this->_calculator->getTokens());
+
+        $this->_calculator->setExpression('sqrt(-5)');
+        $this->assertEquals(['sqrt', '(', '-5', ')'], $this->_calculator->getTokens());
     }
 }
  
