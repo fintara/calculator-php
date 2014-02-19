39ec30967905f6d8e189d3360935139d3ef3fba4
fintara <ccovedenski@gmail.com>
2014-02-19 22:57:49 +0200

    Simplifying tests

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -35,52 +35,21 @@ class CalculatorTest extends \PHPUnit_Framework_TestCase {
     }
 
     public function testGetReversedPolishNotation() {
-        $this->_calculator->setExpression('1+2');
-        $queue = $this->_calculator->getReversePolishNotation($this->_calculator->getTokens());
-        $this->assertEquals('12+', $this->queueToString($queue));
-
-        $this->_calculator->setExpression('1^-2');
-        $queue = $this->_calculator->getReversePolishNotation($this->_calculator->getTokens());
-        $this->assertEquals('1-2^', $this->queueToString($queue));
-
-        $this->_calculator->setExpression('1^(-2)');
-        $queue = $this->_calculator->getReversePolishNotation($this->_calculator->getTokens());
-        $this->assertEquals('1-2^', $this->queueToString($queue));
-
-        $this->_calculator->setExpression('1+2-3');
-        $queue = $this->_calculator->getReversePolishNotation($this->_calculator->getTokens());
-        $this->assertEquals('12+3-', $this->queueToString($queue));
-
-        $this->_calculator->setExpression('3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3');
-        $queue = $this->_calculator->getReversePolishNotation($this->_calculator->getTokens());
-        $this->assertEquals('342*15-23^^/+', $this->queueToString($queue));
-
-        $this->_calculator->setExpression('3 + 4 * 2 / ( 1 - 5 )');
-        $queue = $this->_calculator->getReversePolishNotation($this->_calculator->getTokens());
-        $this->assertEquals('342*15-/+', $this->queueToString($queue));
-
-        $this->_calculator->setExpression('5 + ((1 + 2) * 4) - 3');
-        $queue = $this->_calculator->getReversePolishNotation($this->_calculator->getTokens());
-        $this->assertEquals('512+4*+3-', $this->queueToString($queue));
+        $this->executeGetReversePolishNotation('1+2', '12+');
+        $this->executeGetReversePolishNotation('1^-2', '1-2^');
+        $this->executeGetReversePolishNotation('1^(-2)', '1-2^');
+        $this->executeGetReversePolishNotation('1+2-3', '12+3-');
+        $this->executeGetReversePolishNotation('3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3', '342*15-23^^/+');
+        $this->executeGetReversePolishNotation('3 + 4 * 2 / ( 1 - 5 )', '342*15-/+');
+        $this->executeGetReversePolishNotation('5 + ((1 + 2) * 4) - 3', '512+4*+3-');
     }
 
     public function testCalculate() {
-        $limit = 5;
-
-        $this->_calculator->setExpression('250*14.3');
-        $this->assertEquals(3575, $this->_calculator->calculate($limit));
-
-        $this->_calculator->setExpression('3^6 / 117');
-        $this->assertEquals(6.23077, $this->_calculator->calculate($limit));
-
-        $this->_calculator->setExpression('(2.16 - 48.34)^-1');
-        $this->assertEquals(-0.02165, $this->_calculator->calculate($limit));
-
-        $this->_calculator->setExpression('(59 - 15 + 3*6)/21');
-        $this->assertEquals(2.95238, $this->_calculator->calculate($limit));
-
-        $this->_calculator->setExpression('-(-5)');
-        $this->assertEquals('5', $this->_calculator->calculate($limit));
+        $this->executeCalculation('250*14.3', 3575);
+        $this->executeCalculation('3^6 / 117', 6.23077);
+        $this->executeCalculation('(2.16 - 48.34)^-1', -0.02165);
+        $this->executeCalculation('(59 - 15 + 3*6)/21', 2.95238);
+        $this->executeCalculation('3-(4-6)', 5);
     }
 
     public function testFormatNumber() {
@@ -95,12 +64,20 @@ class CalculatorTest extends \PHPUnit_Framework_TestCase {
         $this->assertEquals('12345.1', $this->_calculator->formatNumber(12345.100001, $limit));
     }
 
-    private function queueToString(\SplQueue $queue) {
+    private function executeCalculation($expression, $expect, $limit = 5) {
+        $this->_calculator->setExpression($expression);
+        $this->assertEquals($expect, $this->_calculator->calculate($limit));
+    }
+
+    private function executeGetReversePolishNotation($expression, $expect) {
+        $this->_calculator->setExpression($expression);
+        $queue = $this->_calculator->getReversePolishNotation($this->_calculator->getTokens());
+
         $result = '';
         while($queue->count() > 0)
             $result .= $queue->dequeue();
 
-        return $result;
+        $this->assertEquals($expect, $result);
     }
 }
  
